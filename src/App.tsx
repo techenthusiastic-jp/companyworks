8521b47ad21b8e040cd6a08636cc2693ed9912cc
JayaPriyaJayaKumarSelvi <116954229+JayaPriyaJayaKumarSelvi@users.noreply.github.com>
2024-06-10 13:31:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,123 @@
+import { WelcomePage, GitHubBanner, Refine, Authenticated } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import {
+ 
+  useNotificationProvider
+} from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import {dataProvider,
+  
+  liveProvider,authProvider,
+} from "./providers";
+import {Home, ForgotPassword,Login,Register,CompanyList} from "./pages";
+import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import routerBindings, {
+  
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import { resources } from "./config/resources";
+import { COMPANIES_LIST_QUERY } from "./graphql/queries";
+import EditTask from "./pages/tasks/edit";
+import CreateTask from "./pages/tasks/create";
+
+
+
+
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                authProvider={authProvider}
+                resources={resources}
+
+              
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "VIKsWr-l8YAuH-Zla8sB",
+                  liveMode: "auto",
+                }}
+              >
+                <Routes>
+                 
+                  
+                    <Route path="/forgot-password" element={<ForgotPassword />}/>
+                    <Route path="/register"element={<Register />}/>
+                    <Route path="/login"element={<Login />}/>
+
+                    <Route
+                    element={
+                    <Authenticated key ="authenticated-layout"
+                    fallback={<CatchAllNavigate to="/login"/>} 
+                   >
+                     <Layout>
+                        <Outlet />
+                      </Layout>
+                    
+                   </Authenticated>
+                     
+
+                    }>
+                       <Route index element={<Home />}/>
+                       <Route path="/companies" >
+                        <Route index element={<CompanyList />} />
+                        <Route path="new" element={<Create />} />
+                        <Route path="edit/:id" element={<Edit />}/>
+                     
+                      </Route> 
+                    <Route path="/tasks" element={
+                    <List>
+                        <Outlet />
+                    </List>
+                  }>
+                     <Route path="new" element={<CreateTask/>} 
+                     />
+                     <Route path="edit/:id" element={<EditTask/>} 
+                     />
+                       </Route>
+                      </Route>
+                     
+                    
+                </Routes>
+
+
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+      
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
